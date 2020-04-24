d77ee8349dd415c76926a765fc28f9bbc53779d2
yuetsin <akaza_akari@sjtu.edu.cn>
2020-04-24 19:24:25 +0800

    [add] skeleton

---
+++ InstaPass/InstaPass/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  InstaPass
+//
+//  Created by 法好 on 2020/4/24.
+//  Copyright © 2020 yuetsin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
