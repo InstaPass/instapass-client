ccf03a771e33bb2a12dee282837f48019e9d6b35
yuetsin <akaza_akari@sjtu.edu.cn>
2020-04-25 22:32:09 +0800

    [fix] qr code light/dark switch

--- InstaPass-iOS/InstaPass/VC/QRCodeViewController.swift
+++ InstaPass-iOS/InstaPass/VC/QRCodeViewController.swift
@@ -15,24 +15,42 @@ class QRCodeViewController: UIViewController {
     @IBOutlet weak var QRCodeView: UIImageView!
     @IBOutlet weak var refreshButton: UIButton!
     
+    var targetUrl: URL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        flushQRCode(url: URL(string: "https://en.wikipedia.org/wiki/Death")!)
+        targetUrl = URL(string: "https://en.wikipedia.org/wiki/Death")
+        flushQRCode(style: self.traitCollection.userInterfaceStyle)
     }
     
-    func flushQRCode(url: URL) {
-        var qrCode = QRCode(url)
+    func flushQRCode(style: UIUserInterfaceStyle? = nil) {
+        if targetUrl == nil {
+            return
+        }
+        
+        var qrCode = QRCode(targetUrl!)
         qrCode?.color = CIColor(rgba: "64d277")
         
+        if style != nil {
+            if style == .dark {
+                qrCode?.backgroundColor = CIColor.black
+            } else if style == .light {
+                qrCode?.backgroundColor = CIColor.white
+            }
+        }
         QRCodeView.image = qrCode?.image
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        flushQRCode(style: self.traitCollection.userInterfaceStyle)
     }
 
     @IBAction func onRefreshButtonTapped(_ sender: UIButton) {
-        flushQRCode(url: URL(string: "https://en.wikipedia.org/wiki/\(arc4random_uniform(1024))")!)
+        targetUrl = URL(string: "https://en.wikipedia.org/wiki/\(arc4random_uniform(1024))")
+        flushQRCode()
     }
-    
 }
 
